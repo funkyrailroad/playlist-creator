8ee3afd7d2f44ae1bfbed412dc2b8ac93a730529
Jake Atwell <jatwell@alexinc.com>
2022-09-25 18:54:57 -0400

    Track class in playlists

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ import unittest
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-from util import Playlist
+from util import Playlist, Track
 
 
 class Tests(unittest.TestCase):
@@ -17,6 +17,14 @@ class Tests(unittest.TestCase):
         )
         self.sp = spotipy.Spotify(auth_manager=oauth_manager)
         self.raw_playlist = self.get_example_raw_playlist()
+        self.raw_track = self.get_example_raw_track()
+        self.playlist = Playlist(self.raw_playlist)
+        self.tracks = self.playlist.tracks
+        self.track = self.tracks[0]
+
+    def get_example_raw_track(self):
+        raw_tracks = self.raw_playlist["tracks"]["items"]
+        return raw_tracks[0]['track']
 
     def get_example_raw_playlist(self):
         playlists = self.sp.current_user_playlists()["items"]
@@ -24,7 +32,15 @@ class Tests(unittest.TestCase):
         return self.sp.playlist(playlist_id)
 
     def test_playlist_attributes(self):
-        playlist = Playlist(self.raw_playlist)
+        playlist = self.playlist
         self.assertEqual(playlist.name, self.raw_playlist['name'])
         self.assertEqual(playlist.id, self.raw_playlist['id'])
         self.assertEqual(playlist.uri, self.raw_playlist['uri'])
+        self.assertIsInstance(playlist.tracks, list)
+        self.assertIsInstance(playlist.tracks[0], Track)
+
+    def test_track_attributes(self):
+        track = self.track
+        self.assertEqual(track.name, self.raw_track['name'])
+        self.assertEqual(track.id, self.raw_track['id'])
+        self.assertEqual(track.uri, self.raw_track['uri'])
--- util.py
+++ util.py
@@ -4,3 +4,23 @@ class Playlist:
         self.name = js['name']
         self.id = js['id']
         self.uri = js['uri']
+        self.tracks = self.get_tracks()
+
+    def get_tracks(self):
+        raw_tracks = self.js["tracks"]["items"]
+        tracks = [Track(rt["track"]) for rt in raw_tracks]
+        return tracks
+
+    def __str__(self):
+        return self.name
+
+
+class Track:
+    def __init__(self, js):
+        self.js = js
+        self.name = js['name']
+        self.id = js['id']
+        self.uri = js['uri']
+
+    def __str__(self):
+        return self.name
