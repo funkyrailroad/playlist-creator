ef0d0f056e9de495ad34e2099149044bcd4d5c07
Jake Atwell <jatwell@alexinc.com>
2022-09-25 20:33:31 -0400

    Update main

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 
+from util import Playlist, Track, get_bpms
+
 auth_manager = SpotifyClientCredentials(
     client_id=os.environ["SPOTIPY_CLIENT_ID"],
     client_secret=os.environ["SPOTIPY_CLIENT_SECRET"],
@@ -20,31 +22,24 @@ sp = spotipy.Spotify(auth_manager=auth_manager)
 playlists = sp.current_user_playlists()["items"]
 
 playlist_id = playlists[0]["id"]
-playlist = sp.playlist(playlist_id)
-tracks = playlist["tracks"]["items"]
-
-
-track_ids = [track["track"]["id"] for track in tracks]
-
-audio_features = sp.audio_features(track_ids)
-bpms = [af["tempo"] for af in audio_features]
-assert len(track_ids) == len(bpms)
-
+raw_playlist = sp.playlist(playlist_id)
+playlist = Playlist(raw_playlist)
+raw_tracks = raw_playlist["tracks"]["items"]
+tracks = [Track(rt["track"]) for rt in raw_tracks]
+bpms = get_bpms(tracks)
 
 min_bpm = 100
 max_bpm = 110
 
 filtered_songs = []
-for track_id, bpm in zip(track_ids, bpms):
-    if (bpm >= min_bpm) and (bpm <= max_bpm):
-        filtered_songs.append(track_id)
+for track, bpm in zip(tracks, bpms):
+    if min_bpm <= bpm <= max_bpm:
+        filtered_songs.append(track)
 
-# TODO: create a new playlist with just these filtered songs
+# TODO: create a new raw_playlist with just these filtered songs
 # user_playlist_create(user, name, public=True, collaborative=False, description='')
 
-
-# add items to playlist
+# add items to raw_playlist
 # playlist_add_items(playlist_id, items, position=None)
-
 
 breakpoint()
